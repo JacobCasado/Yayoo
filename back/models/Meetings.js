c93ef4646e386313107a58ed106eab786d3eab9a
Jacob Casado <jacob.casado.perez@gmail.com>
2018-10-09 16:31:39 +0200

    Modelos y Auth

---
+++ back/models/Meetings.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const meetingsSchema = new Schema({
+  place: String,
+  location: {type: { type: String }, coordinates: [Number]},
+  date: Date,
+  name: String,
+  description: String,
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const Meetings = mongoose.model('Meetings', meetingsSchema);
+module.exports = Meetings;
---
+++ front/src/components/AuthService.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+class AuthService {
+  constructor() {
+    this.service = axios.create({
+      baseURL: 'http://localhost:3010/api/auth',
+      withCredentials: true
+    });
+  }
+
+  signup = (username, password) => {
+    return this.service.post('/signup', {username, password})
+    .then(response => response.data)
+  }
+
+  login = (username, password) => {
+    return this.service.post('/login', {username, password})
+    .then(response => response.data)
+  }
+
+  loggedin = () => {
+    return this.service.get('/currentUser',)
+    .then(response => response.data)
+  }
+
+  logout = () => {
+    return this.service.get('/logout',)
+    .then(response => response.data)
+  }
+
+  upload = (file) => {
+    return this.service.post('/upload', {file})
+    .then(response => response.data)
+  }
+
+
+  edit = (username, password, campus, course) => {
+    return this.service.post('/edit', {username, campus, course})
+    .then(response => response.data)
+  }
+}
+
+export default AuthService;
